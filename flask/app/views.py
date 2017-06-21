6d974825e19d9ff36a5b3b1f735e9e05b5edec2e
Levon <Levon@engr.uconn.edu>
2017-06-21 16:44:42 -0400

    Updated the flask app.

--- flask/app/views.py
+++ flask/app/views.py
@@ -59,31 +59,6 @@ def db_connection():
 def index():
   return render_template('index.html')
 
-@app.route('/hello_world')
-def hello_world():
-    return 'Hello, World (hello_world)!'
-
-
-@app.route('/user/<username>')
-def show_user_profile(username):
-    # show the user profile for that user
-    return 'User %s' % username
-
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    # show the post with the given id, the id is an integer
-    return 'Post %d' % post_id
-
-
-@app.route('/projects/')
-def projects():
-    return 'The project page'
-
-@app.route('/about')
-def about():
-    return 'The about page'
-
-
 @app.route('/db_enc')
 def db_enc():
     cursor = db_connection()
